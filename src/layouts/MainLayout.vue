b37aa2516ec4a0d9fbca0f1fed69ff7a3ace1a37
breno.lima <brenoryan21@hotmail.com>
2024-08-07 01:04:02 -0300

    feat: Implementando back to top

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -26,6 +26,9 @@
     </q-drawer>
 
     <q-page-container>
+      <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
+        <q-btn fab icon="keyboard_arrow_up" color="accent" />
+      </q-page-scroller>
       <router-view />
     </q-page-container>
   </q-layout>
